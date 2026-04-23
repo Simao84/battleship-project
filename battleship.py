4c4e5ee3319d8bf3d5660b37c51a74b7130c671f
simao garcia <simao.garcia.sdg@gmail.com>
2026-04-23 11:00:40 +0200

    Add shot counter player

--- battleship.py
+++ battleship.py
@@ -75,7 +75,8 @@ class BattleshipGameState:
     def __str__(self) -> str:
         outstring = ""
         for idx in [0, 1]:
-            outstring += f"----------- Player {idx + 1} -----------\n"
+            shots_fired = len(self.players[idx].shots)
+            outstring += f"----------- Player {idx + 1} (Shots: {shots_fired}) -----------\n"
             if self.winner == idx:
                 outstring += Back.GREEN + Fore.YELLOW + Style.BRIGHT + "Winner!" + Style.RESET_ALL + "\n"
             elif self.idx_player_active == idx:
